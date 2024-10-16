8503e4f64e45d2f610ca8b80427be755769d0fe8
Amanda Bueno <149975225+amandabuenosm@users.noreply.github.com>
2024-10-16 14:00:11 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,90 +1,82 @@
-// public class Contato {
-//     private String nome;
-//     private String fone;
-//     private String end;
+import java.util.Scanner;
 
-//     public Contato(String nome, String fone, String end) {
-//         this.nome = nome;
-//         this.fone = fone;
-//         this.end = end;
-//     }
+public class Main {
+    public static void main(String[] args) {
+        Agenda agenda = new Agenda("data/contatos.txt");
+        Scanner scanner = new Scanner(System.in);
 
-//     public String getNome() {
-//         return nome;
-//     }
+        int opcaouser;
+  
+        do {
+            System.out.println("-----------------------------------");
+            System.out.println("Menu do Usuário da Agenda:");
+            System.out.println("1 - Importar Contatos");
+            System.out.println("2 - Salvar Contatos");
+            System.out.println("3 - Criar novo Contato");
+            System.out.println("4 - Excluir Contato por Nome");
+            System.out.println("5 - Buscar Contato por Nome");
+            System.out.println("6 - Buscar Contato por Telefone");
+            System.out.println("7 - Listar todos os Contatos");
+            System.out.println("8 - Limpar Agenda");
+            System.out.println("9 - Editar Contato");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção:");
+            opcaouser = scanner.nextInt();
+            scanner.nextLine();
 
-//     public String getTelefone() {
-//         return fone;
-//     }
+            switch (opcaouser) {
+                case 1:
+                    agenda.importararquivo();
+                    break;
+                case 2:
+                    agenda.salvarcontato();
+                case 3:
+                    System.out.println("-----------------------------------");
+                    System.out.print("Nome: ");
+                    String nome = scanner.nextLine();
+                    System.out.print("Telefone: ");
+                    String fone = scanner.nextLine();
+                    System.out.print("Endereço: ");
+                    String end = scanner.nextLine();
+                    agenda.novocontato(nome, fone, end);
+                    break;
+                case 4:
+                    System.out.print("Nome: ");
+                    String deletanome = scanner.nextLine();
+                    agenda.deletapornome(deletanome);
+                    break;
+                case 5:
+                    System.out.print("Nome: ");
+                    System.out.println("-----------------------------------");
+                    String buscapornome = scanner.nextLine();
+                    agenda.contatospornome(buscapornome);
+                    break;
+                case 6:
+                    System.out.print("Telefone: ");
+                    String buscaporfone = scanner.nextLine();
+                    agenda.contatoporfone(buscaporfone);
+                    break;
+                case 7:
+                    agenda.listadecontato();
+                    break;
+                case 8:
+                    agenda.limpezadeagenda();
+                    break;
+                case 9: 
+                System.out.print("Nome: ");
+                String editarpornome = scanner.nextLine();
+                    agenda.editarcontato(editarpornome);
+                    break;
+                case 0:
+                    System.out.println("-----------------------------------");
+                    System.out.print("ENCERRANDO AGENDA DE CONTATOS...");
+                    break;
+                default:
+                    System.out.print("OPÇÃO INVÁLIDA! TENTE DE NOVO!\n");
+                    break;
+                } 
+            } while (opcaouser != 11);
 
-//     public String getEndereco() {
-//         return end;
-//     }
-
-//     @Override
-//     public String toString() {
-//         return "----------------------------------" +
-//         "\nNome: " + nome +
-//         "\nTelefone: " + fone +
-//         "\nEndereço: " + end;
-//     }
-
-//     public void setNome(String novoNome) {
-//         // TODO Auto-generated method stub
-//         throw new UnsupportedOperationException("Unimplemented method 'setNome'");
-//     }
-
-//     public void setTelefone(String novoTelefone) {
-//         // TODO Auto-generated method stub
-//         throw new UnsupportedOperationException("Unimplemented method 'setTelefone'");
-//     }
-
-//     public void setEndereco(String novoEndereco) {
-//         // TODO Auto-generated method stub
-//         throw new UnsupportedOperationException("Unimplemented method 'setEndereco'");
-//     }
-// }
-
-
-public class Contato {
-    private String nome;
-    private String telefone;
-    private String endereco;
-
-    public Contato(String nome, String telefone, String endereco) {
-        this.nome = nome;
-        this.telefone = telefone;
-        this.endereco = endereco;
-    }
-
-    // Getters
-    public String getNome() {
-        return nome;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    // Setters
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    @Override
-    public String toString() {
-        return "Nome: " + nome + ", Telefone: " + telefone + ", Endereço: " + endereco;
+            scanner.close();
     }
 }
